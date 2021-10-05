3ee183042400b8e28ca14fcf2324d707b0b7b7b6
Mene-hub <menegotto.claudio@gmail.com>
2021-10-05 17:58:53 +0200

    added download buttons

--- assets/js/connection.js
+++ assets/js/connection.js
@@ -1,7 +1,7 @@
 function gitConnection(){
 	var get = new XMLHttpRequest();
 	// 2. Configure it: GET-request for the URL /article/.../load
-	get.open('GET', 'https://api.github.com/users/Mene-hub/repos');
+	get.open('GET', 'https://api.github.com/users/Mene-hub/repos', true);
 
 	// 3. Send the request over the network
 	get.send();
@@ -15,9 +15,32 @@ function gitConnection(){
 			var template;
             for(var i = 0; i<GitRepose.length; i++)
 			{
-				template = "<div class='col-md-6 col-lg-4'> <div class='project-card-no-image'> <h3>" + GitRepose[i].name + "</h3> <h4>" + GitRepose[i].description + "</h4><a class='Git_Button' role='button' href='" + GitRepose[i].html_url + "' Target='Blank'>more</a></div> </div></div>";
+				template = "<div class='col-md-6 col-lg-4'> <div class='project-card-no-image' id='" + GitRepose[i].name + "' > <h3>" + GitRepose[i].name + "</h3> <h4>" + GitRepose[i].description + "</h4><a class='Git_Button' role='button' href='" + GitRepose[i].html_url + "' Target='Blank'>more</a></div></div></div>";
 				var tmp = document.getElementById("reposeContainer");
 				tmp.innerHTML = tmp.innerHTML + template;
+				gitReleases(GitRepose[i].name);
+			}
+		}
+	}
+}
+
+function gitReleases(repos){
+	var get = new XMLHttpRequest();
+	// 2. Configure it: GET-request for the URL /article/.../load
+	var repos_apiUrl ="https://api.github.com/repos/Mene-hub/" + repos + "/releases";
+	get.open('GET', repos_apiUrl, true);
+
+	// 3. Send the request over the network
+	get.send();
+
+	// 4. This will be called after the response is received
+	get.onload = function() {
+		if (get.status != 200) { // analyze HTTP status of the response
+			console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+		} else { // show the result
+			if(JSON.parse(get.response).length>0){
+				url = JSON.parse(get.response)[0].assets[0].browser_download_url;
+				document.getElementById(repos).innerHTML += "<a class='Git_Download' role='button' href='" + url + "' Target='Blank'>download</a>";
 			}
 		}
 	}
